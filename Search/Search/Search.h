e49c44f859f545b15dd1b66f8987e8b860d7287e
ChemistQin <365983009@qq.com>
2016-11-04 00:15:19 +0800

    二分

--- Search/Search/Search.h
+++ Search/Search/Search.h
@@ -9,5 +9,33 @@
 #ifndef Search_h
 #define Search_h
 
+using namespace std;
+
+//顺序查找
+void OrderSearch(vector<int> &vec , int key){
+    int i = 0;
+    while (i < vec.size() && vec[i] != key) {
+        i++;
+        if(vec[i] == key)
+            cout << key << " in No." << i <<endl;
+        if(i >= vec.size())
+            cout << key <<" Not Found \n" ;
+    }
+}
+
+//折半查找
+void BinarySearch(vector<int> &vec , int key){
+    int low = 0 , high = vec.size() - 1 ;
+    int mid = (low + high) >> 1;
+    while (low <= high && vec[mid] != key) {
+        mid = (low + high)/2;
+        if(vec[mid] == key)
+            cout << key << " in No." << mid  <<endl;
+        if(vec[mid] > key)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+}
 
 #endif /* Search_h */
--- Search/Search/main.cpp
+++ Search/Search/main.cpp
@@ -14,5 +14,13 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     vector<int> vec = {2,4,8,16,32,64,128,256,512,1024};
+    OrderSearch(vec,128);
+    cout << endl;
+    OrderSearch(vec,2048);
+    cout << endl;
+    //BinarySearch(vec,64);
+    cout << endl;
+    //BinarySearch(vec,2048);
+    cout << endl;
     return 0;
 }
