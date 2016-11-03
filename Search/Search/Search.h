96b2ec1b9cf615bac3cdf6c8983a776620a774b7
ChemistQin <365983009@qq.com>
2016-11-03 23:06:59 +0800

    search

---
+++ Search/Search/Search.h
@@ -0,0 +1,13 @@
+//
+//  Search.h
+//  Search
+//
+//  Created by 覃思平 on 2016/11/3.
+//  Copyright © 2016年 覃思平. All rights reserved.
+//
+
+#ifndef Search_h
+#define Search_h
+
+
+#endif /* Search_h */
---
+++ Search/Search/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Search
+//
+//  Created by 覃思平 on 2016/11/3.
+//  Copyright © 2016年 覃思平. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include "Search.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    vector<int> vec = {2,4,8,16,32,64,128,256,512,1024};
+    return 0;
+}
