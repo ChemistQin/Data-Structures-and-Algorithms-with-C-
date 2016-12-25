d7a8a2e052308addeb24efdd2b5f8cec539525ad
ChemistQin <365983009@qq.com>
2016-12-25 22:56:27 +0800

    --408其实不是很难--

---
+++ Template/Template/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Template
+//
+//  Created by 覃思平 on 2016/12/25.
+//  Copyright © 2016年 覃思平. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
